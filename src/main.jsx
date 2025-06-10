71ce2ec3dd44cd56f040cca0b4133539a9e51e73
Shadowheart ~ <sodhadharmraj30@gmail.com>
2025-06-10 12:52:51 +0530

    fix hope

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <App />
-)
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
