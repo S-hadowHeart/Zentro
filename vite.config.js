4892f4f5b28211615b27736f0670e649f538a616
Shadowheart ~ <sodhadharmraj30@gmail.com>
2025-06-10 11:43:23 +0530

    fix hope

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': ['react', 'react-dom', 'react-router-dom'],
+          'auth': ['./src/contexts/AuthContext.jsx'],
+          'tasks': ['./src/contexts/TasksContext.jsx']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000,
+    sourcemap: true
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'react-router-dom']
+  }
 })
